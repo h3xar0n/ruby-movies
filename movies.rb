8d19e724ca6cd9acd6b4f071a916b374bbb2d0d9
h3xar0n <aron@glambyadelina.com>
2017-04-14 22:12:15 -0400

    Case statement

--- movies.rb
+++ movies.rb
@@ -1,5 +1,18 @@
 movies = {
-    'Empire Strikes Back' => '4'
+  'Empire Strikes Back' => '4'
 }
 puts "What is your favorite movie?"
 answer = gets.chomp
+
+case choice
+when "add"
+    puts "Added!"
+when "update"
+    puts "Updated!"
+when "display"
+    puts "Movies!"
+when "delete"
+    puts "Deleted!"
+else
+    puts "Error!"
+end
