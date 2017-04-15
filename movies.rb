c1e37f00f6f900158219e814b837b466929a9d03
h3xar0n <aron@glambyadelina.com>
2017-04-14 22:18:40 -0400

    Converting to the right types with to_sym and to_i

--- movies.rb
+++ movies.rb
@@ -1,18 +1,23 @@
 movies = {
   'Empire Strikes Back' => '4'
 }
-puts "What is your favorite movie?"
-answer = gets.chomp
+puts "What would you like to do, add, update, display, or delete?"
+choice = gets.chomp
 
 case choice
 when "add"
-    puts "Added!"
+  puts "What is the title of the movie you want to add?"
+  title = gets.chomp.to_sym
+  puts "On a scale of 1 to 4, how would you rate the movie?"
+  rating = gets.chomp.to_i
+  movies[title] = rating
+  puts movies[title]
 when "update"
-    puts "Updated!"
+  puts "Updated!"
 when "display"
-    puts "Movies!"
+  puts "Movies!"
 when "delete"
-    puts "Deleted!"
+  puts "Deleted!"
 else
-    puts "Error!"
+  puts "Error!"
 end
